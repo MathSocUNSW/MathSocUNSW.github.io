414d156a7afeb90551c2dce6b0d4b5573c2a8eb1
Ethan Brown <z5162332@student.unsw.edu.au>
2020-02-09 15:43:50 +1100

    Changed pw on request of James Gao

--- _pages/unlock.php
+++ _pages/unlock.php
@@ -2,6 +2,6 @@
 permalink: /oweek/unlock.php
 ---
 <?php
-if ($_POST['p'] === '42069')
+if ($_POST['p'] === 'semendemon69')
     echo 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSegCEw6ZGEl2kUVnK_kN7CK1e4HdQU9LqDgSXJCGg5-HQV6gg/formResponse';
 ?>
--- docs/oweek/unlock.php
+++ docs/oweek/unlock.php
@@ -1,4 +1,4 @@
 <?php
-if ($_POST['p'] === '42069')
+if ($_POST['p'] === 'semendemon69')
     echo 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSegCEw6ZGEl2kUVnK_kN7CK1e4HdQU9LqDgSXJCGg5-HQV6gg/formResponse';
 ?>
