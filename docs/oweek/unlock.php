53c0298d82da4447dc485e2cb4fad5532d353d03
Ethan Brown <z5162332@student.unsw.edu.au>
2020-02-09 15:59:12 +1100

    Sync

--- docs/oweek/unlock.php
+++ docs/oweek/unlock.php
@@ -1,4 +1,4 @@
 <?php
-if ($_POST['p'] === 'semendemon69')
+if ($_POST['p'] === 'math0week')
     echo 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSegCEw6ZGEl2kUVnK_kN7CK1e4HdQU9LqDgSXJCGg5-HQV6gg/formResponse';
 ?>
